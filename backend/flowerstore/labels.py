dde48753eb174c3963199cf820bd469a7153c68e
abaz47 <alexander.bazarov@gmail.com>
2023-11-28 13:05:33 +0300

    edit footer

--- backend/flowerstore/labels.py
+++ backend/flowerstore/labels.py
@@ -1,7 +1,9 @@
 LABELS = {
+    'author': 'made by baz00ka',
     'bouquets': 'Букеты',
     'constructor': 'Собрать букет',
     'delivery': 'Доставка',
+    'footer': 'Футер',
     'phone': '+71234567890',
     'phone_label': ' +7 (123) 456-78-90',
     'portfolio': 'Наши работы',
