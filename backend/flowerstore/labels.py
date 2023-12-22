987fdad960448aab1d8f7ec20dbe2b6b0bc8f1ff
abaz47 <alexander.bazarov@gmail.com>
2023-12-22 18:24:49 +0300

    add flower card template, edit index, showcase

--- backend/flowerstore/labels.py
+++ backend/flowerstore/labels.py
@@ -4,9 +4,13 @@ LABELS = {
     'constructor': 'Собрать букет',
     'delivery': 'Доставка',
     'footer': 'Футер',
+    'index_content': '* * *',
+    'index_title': 'Главная',
     'phone': '+71234567890',
     'phone_label': ' +7 (123) 456-78-90',
     'portfolio': 'Наши работы',
     'shop': 'Магазин цветов',
     'showcase': 'Цветы',
+    'showcase_description': 'В наличии',
+    'showcase_header': 'Наши цветы'
 }
